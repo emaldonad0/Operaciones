12314e206f556018fd5c389ec3bf60e14a6ce6a1
Eduardo Maldonado <eduardo.maldonado@cfe.mx>
2024-02-27 14:58:32 -0600

    Add function for subtract

--- src/main/java/mx/cfe/Operar.java
+++ src/main/java/mx/cfe/Operar.java
@@ -5,4 +5,8 @@ public class Operar {
     public Double sumar(Double numero1, Double numero2) {
         return numero1+numero2;
     }
+
+    public Double restar(Double numero1, Double numero2){
+        return numero1-numero2;
+    }
 }
